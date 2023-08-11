599de3eb533f6e2c2bad090eb73fd35c994609a6
Kevlezama <46420852+kevlezama@users.noreply.github.com>
2023-08-11 12:30:04 -0300

    adding new files

--- Learning/POO/Student.py
+++ Learning/POO/Student.py
@@ -1,3 +1,13 @@
-import Person
+from abstract_classes import Person
 
-x = Person()
+
+class Student(Person):
+    def __init__(self, student_name, student_last_name, student_dni, student_age, student_birthday):
+        self.student_name = student_name
+        self.s_last_name = student_last_name
+        self.s_dni = student_dni
+        self.s_age = student_age
+        self.s_birthday = student_birthday
+
+    def get_full_student_data(self):
+        return self.student_name
--- Learning/POO/person_implementation.py
+++ Learning/POO/person_implementation.py
@@ -1,7 +1,10 @@
-class Person():
+from abstract_classes import Person
+
+class Person_imp(Person):
     
     def __init__(self, p_first_name, p_last_name, p_dni,
                   p_personal_phone:int = None, p_middle_name = None,p_address = None):
+        super.__init__()
         self.p_first_name = p_first_name
         self.p_middle_name = p_middle_name
         self.p_last_name = p_last_name
@@ -13,6 +16,9 @@ class Person():
 
         return f"Basic details:\n Name: {self.p_first_name}\n Lastname: {self.p_last_name}\n DNI: {self.p_dni}"
     
+    def get_report(self):
+        pass
+
     def __repr__(self) -> str:
         ## __class__ return class format name. __class__.___name___ return class str name
         return f"{self.__class__}(Name={self.p_first_name}) " f" {self.__class__. __name__}(Name={self.p_first_name})"
@@ -30,3 +36,6 @@ if __name__ == '__main__':
 people = {95851445:["Kevin","Lezama"], 97436212:["Valeria","Abad"]}
 
 '''rslt = [Person(arg1,arg2,arg3) for arg1 in people[95851445]]'''
+
+
+
