91775b96a8a2142614e1d46ebc3570c3be74e9b3
Kevlezama <46420852+kevlezama@users.noreply.github.com>
2023-08-12 14:34:29 -0300

    adding

--- Learning/POO/super_classes/Person_super_class.py
+++ Learning/POO/super_classes/Person_super_class.py
@@ -1,9 +1,11 @@
-from abc import ABC, abstractmethod
+class Person():
 
-class Person(ABC):
-    
-    def __init__(self, p_first_name: str, p_middle_name: str, p_last_name: str, p_dni: int,
-                  p_personal_phone:int ,p_address:str) -> None:
+    def __init__(self, p_first_name: str,
+                p_last_name: str,
+                p_dni: int,
+                p_personal_phone:int = None, 
+                p_middle_name:str = None,
+                p_address:str = None) -> None:
         self.p_first_name = p_first_name
         self.p_middle_name = p_middle_name
         self.p_last_name = p_last_name
@@ -11,6 +13,7 @@ class Person(ABC):
         self.p_address = p_address
         self.p_personal_phone = p_personal_phone
 
-    @abstractmethod
     def get_person_basic_details(self):
-        raise NotImplementedError
+        pass
+
+    
